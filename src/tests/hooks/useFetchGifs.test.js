28924fd187c6ff081e8919e9378f79cffe3b1e8a
Allan Montilla <allangmr7@gmail.com>
2021-08-08 14:40:16 -0500

    Se actualizan archivos de test con jest and enzyme

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,20 +2,22 @@ import { useFetchGifs } from "../../hooks/useFetchGifs"
 import {renderHook} from '@testing-library/react-hooks'
 
 describe('Test of Hook useFetchGifs', () => {
-    test('should be return the initial state', () => {
-        const {result} = renderHook(()=>useFetchGifs('Pokemon') )
-        const {data, loading} = result.current;
+    test('should be return the initial state', async() => {
 
+        const {result,waitForNextUpdate} = renderHook(()=>useFetchGifs('Pokemon') )
+        const {data, loading} = result.current;
+        await waitForNextUpdate({timeout:1500});
 
         expect(data).toEqual([]);
         expect(loading).toEqual(true);
     })
 
     test('should return img and loading in false', async() => {
-        const {result,waitForNextUpdate} = renderHook(()=>useFetchGifs('Pokemon') )
+        const {result,waitForNextUpdate} = renderHook(()=>useFetchGifs('One Punch') )
+        await waitForNextUpdate({timeout:1500});
         const {data, loading} = result.current;
-        expect(data).toEqual([]);
-        expect(loading).toEqual(true);
+        expect(data.length).toBe(12);
+        expect(loading).toEqual(false);
     }) 
     
 })
